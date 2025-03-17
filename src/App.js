d2ea6a12cdb77e1c65978404bdbfb93e2008b44b
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-03-17 18:18:26 -0300

    Corrigido problema de falta de importação do tema e icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import {PrimeReactProvider} from "primereact/api";
 import AppRoutes from "./Routes"; // Importa as rotas
+import "primereact/resources/themes/lara-light-blue/theme.css";
+import 'primeicons/primeicons.css';
 
 function App() {
     return (
