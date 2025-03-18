9bad070c55c8807f52455b8892692266fb826484
Yan Pedro <yanpzr1@gmail.com>
2025-03-18 13:44:03 -0300

    feat: implementação de página para solicitar nova senha

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Login from "./pages/login/Login";
 import NotFound from "./pages/notFound/NotFound";
+import RequestNewPassword from "./pages/requestNewPassword/RequestNewPassword";
 
 const AppRoutes = () => {
     return (
@@ -9,6 +10,7 @@ const AppRoutes = () => {
                 <Route path="/" element={<Login/>}/>
                 {/*<Route path="/about" element={<About/>}/>*/}
                 <Route path="*" element={<NotFound/>}/>
+                <Route path="/novasenha" element={<RequestNewPassword/>}/>
             </Routes>
         </Router>
     );
