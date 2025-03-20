1460a64981691d4c08ea189f5a6b40342822e986
lucasrodriguescunha <123827882+lucasrodriguescunha@users.noreply.github.com>
2025-03-20 19:53:30 -0300

    chore(NotFound): adiciona React ao componente

--- src/pages/notFound/NotFound.js
+++ src/pages/notFound/NotFound.js
@@ -1,6 +1,7 @@
-import {Button} from 'primereact/button';
-import {Card} from 'primereact/card';
-import {useNavigate} from 'react-router-dom';
+import React from "react";
+import {Button} from "primereact/button";
+import {Card} from "primereact/card";
+import {useNavigate} from "react-router-dom";
 
 import "./NotFound.css";
 
@@ -11,8 +12,8 @@ const NotFound = () => {
         <div className="not-found-container">
             <Card className="not-found-content" title={<span className="not-found-title">404</span>}>
                 <p className="not-found-message">Página não encontrada</p>
-                <Button label={<span style={{color: 'white'}}>Voltar para a página inicial</span>} icon="pi pi-home"
-                        className="p-button-primary" onClick={() => navigate('/')}/>
+                <Button label={<span style={{color: "white"}}>Voltar para a página inicial</span>} icon="pi pi-home"
+                        className="p-button-primary" onClick={() => navigate("/")}/>
             </Card>
         </div>
     );
